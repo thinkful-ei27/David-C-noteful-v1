f319a2bdc702e7513175cdefdf3f83830de4b7ce
dhcohn <davidhcohn@gmail.com>
2018-12-17 13:29:52 -0500

    sdij

--- server.js
+++ server.js
@@ -5,4 +5,14 @@ const data = require('./db/notes');
 
 console.log('Hello Noteful!');
 
-// INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
