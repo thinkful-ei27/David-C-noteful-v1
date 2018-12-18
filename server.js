a3db7d097eb774addf974fed462834db1f520bb8
dhcohn <davidhcohn@gmail.com>
2018-12-18 15:08:43 -0500

    next thing

--- server.js
+++ server.js
@@ -48,7 +48,22 @@ app.use(function (req, res, next) {
   res.status(404).json({ message: 'Not Found' });
 });
 
-{}
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next){
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
 
 app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
