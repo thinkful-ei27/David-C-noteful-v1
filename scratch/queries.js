55bafd38f39be3e35779eb47b07681fb686789f1
dhcohn <davidhcohn@gmail.com>
2018-12-18 15:25:54 -0500

    next

---
+++ scratch/queries.js
@@ -0,0 +1,51 @@
+const data = require('../db/notes');
+const simDB = require('../db/simDB');
+const notes = simDB.initialize(data);
+
+// GET Notes with search
+notes.filter('cats', (err, list) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(list);
+});
+
+// GET Notes by ID
+notes.find(1005, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+// PUT (Update) Notes by ID
+const updateObj = {
+  title: 'New Title',
+  content: 'Blah blah blah'
+};
+
+notes.update(1005, updateObj, (err, item) => {
+  if (err) {
+    console.error(err);
+  }
+  if (item) {
+    console.log(item);
+  } else {
+    console.log('not found');
+  }
+});
+
+notes.create({title: 'newest title', content: 'so new'}, (err, item) => {
+    if (err) {
+      console.error(err);
+    }
+    if (item) {
+      console.log(item);
+    } else {
+      console.log('not okay');
+    }
+  });
